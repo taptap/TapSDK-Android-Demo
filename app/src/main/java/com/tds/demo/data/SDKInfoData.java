c3b4e1e589eb4db1b9ffc2aba20ff5ba631b3614
yuwenjian <740225978@qq.com>
2022-12-05 11:26:34 +0800

    正版验证增加 Android 11 适配代码

--- app/src/main/java/com/tds/demo/data/SDKInfoData.java
+++ app/src/main/java/com/tds/demo/data/SDKInfoData.java
@@ -17,10 +17,7 @@ public class SDKInfoData {
     // 必须，开发者中心 > 你的游戏 > 游戏服务 > 基本信息 > 域名配置 > API
     public static String SDK_SERVER_URL = "=====换成你的 SERVER_URL ===== ";
 
-
     // 游戏好友中 邀请落地页中的域名  如果是使用的云引擎部署，请在开发者中心=》游戏服务 =》云服务 =》云引擎 =》设置 页面中自定义域名
     public static String Clound_SHAREHOST = "=====换成你的 自定义域名 ===== ";
-
-
 
 }
